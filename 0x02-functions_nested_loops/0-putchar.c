fe2cffa86472c28453487937798fd89d34468fd6
Mercy Runo <mercyruno@gmail.com>
2022-07-05 04:43:20 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	char answer[] = "_putchar\n";
+
+	putchar(answer[]);
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains all the tasks in the functions and nested loops subtopic.
