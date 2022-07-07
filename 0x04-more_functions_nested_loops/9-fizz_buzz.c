8e1fed3a6a59959403306b2f8ad4beef8a2cf955
Mercy Runo <mercyruno@gmail.com>
2022-07-07 11:31:17 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,14 +15,14 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz\n");
+if ((i % 15) == 0)
+printf("FizzBuzz ");
 else if (i % 3 == 0)
-printf("Fizz\n");
+printf("Fizz ");
 else if (i % 5 == 0)
-printf("Buzz\n");
+printf("Buzz ");
 else
-printf("%d\n", i);
+printf("%d ", i);
 }
 printf("\n");
 return (0);
