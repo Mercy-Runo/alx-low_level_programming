43672d67adccd40464e3f125c772c814ac92afe4
Mercy Runo <mercyruno@gmail.com>
2022-07-11 04:29:29 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of arguement to 98
+ * @n: value whose pointer is to be reassigned
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains all tasks of pointers arrays and strings in the c low level programming course.
