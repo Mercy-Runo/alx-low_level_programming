19409bc788f05feb04e89665b90eef0a23a67e40
Mercy Runo <mercyruno@gmail.com>
2022-07-11 06:45:37 -0700

    Half the lies they tell about me

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,34 @@
-#include <stdio.h>
 #include "main.h"
-#include <string.h>
 
 /**
- * puts2 - reverses string
- * @str: string to be reversed
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*s != '\0')
+{
+len++;
+s++;
+}
+
+return (len);
+}
+
+/**
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
  */
 
 void puts2(char *str)
 {
 int i;
 
-while (str[i] != '\0')
-{
-for (i = 0; i <= strlen(str); i += 2)
-{
+for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
 _putchar(str[i]);
-}
-i++;
-}
 _putchar('\n');
 }
