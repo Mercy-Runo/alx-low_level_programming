d880fe2a819e3f5fa5083b792260cf4605327d20
Mercy Runo <mercyruno@gmail.com>
2022-07-11 05:25:27 -0700

    I do not fear computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints to standard output
+ * @str: string being outputted
+ */
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+_putchar('\n');
+*str++;
+}
+}
