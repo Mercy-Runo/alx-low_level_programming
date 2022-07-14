c7041148fbad4c53d1ac61c54621526959ceb4d4
Mercy Runo <mercyruno@gmail.com>
2022-07-14 02:38:01 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,6 +11,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-_strcat(dest, src);
-return (_strcat(dest, src));
+int i, j;
+
+i = 0;
+j = 0;
+
+while (dest[i] != '\0')
+i++;
+
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+
+dest[i] = '\0';
+return (dest);
 }
