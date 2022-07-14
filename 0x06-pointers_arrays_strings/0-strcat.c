2663589e138dfff353f979b03113fc98331333d8
Mercy Runo <mercyruno@gmail.com>
2022-07-14 02:26:48 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcat - concantenates contents of arg 2 to those of arg 1
+ * @dest: arg 1
+ * @src: arg 2
+ * Return: concantenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+_strcat(dest, src);
+return (_strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains all tasks of the pointers arrays and strings subtopic of the c low level programming course.
