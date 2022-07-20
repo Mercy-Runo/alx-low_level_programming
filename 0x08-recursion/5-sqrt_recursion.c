5b44e6c4e0acbcb0d4f3efec71056f8e427b5a90
Mercy Runo <mercyruno@gmail.com>
2022-07-20 04:45:25 -0700

    returns natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,48 +1,41 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
+ * is_squre_root - determines the square root of
+ * @j: an input value
+ * @i: input value
  *
- * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
- *
- * Return: square root of @n or -1
-*/
+ * Description: determines the square root
+ * Return: returns the input val
+ */
 
-int _sqrt_recursion_wrapper(int n, int min, int max)
+int is_square_root(int i, int j)
 {
-	int guess, guess_squared;
-
-	guess = (min + max) / 2;		/*get guess (g)*/
-	guess_squared = guess * guess;		/*square guess (sg)*/
-
-	if (guess_squared == n)			/*if squared guess == n return guess*/
-		return (guess);
-	else if (min == max)			/*this means n doesn't have a perfect square*/
+	if (j * j > i)
+	{
 		return (-1);
-	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/*undershoot g if sg is more than the n*/
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
+	}
+	if (j * j == i)
+	{
+		return (j);
+	}
+	return (is_square_root(i, j + 1));
 }
 
 /**
- * _sqrt_recursion - a function that returns the natural
- *                   square root of a number
+ * _sqrt_recursion - returns square root
+ * @n: input size
  *
- * @n: input number
- *
- * Return: square root
-*/
+ * Description: If n does not have a natural square root,
+ * the function should return -1
+ * Return: returns the natural square root of a number
+ */
+
 int _sqrt_recursion(int n)
 {
-	if (n == 1)		/*sqrt(1) == 1*/
-		return (1);
-	else if (n == 0)	/*sqrt(0) == 0*/
-		return (0);
-	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
+	if (n < 0)
+	{
 		return (-1);
-	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+	}
+	return (is_square_root(n, 1));
 }
