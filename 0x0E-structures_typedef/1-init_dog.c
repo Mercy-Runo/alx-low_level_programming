f1c58242084179497beab38b6a18efd7680612ce
Mercy Runo <mercyruno@gmail.com>
2022-08-01 05:33:09 -0700

    my dog is initialised

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog struct
+ *
+ * @d: input pointer to struct dog
+ * @name: input for dog name
+ * @age: input for dog age
+ * @owner: input for dog owner
+ *
+ * Return: nothing
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+/**
+ * struct dog - dog description
+ */
+struct dog
+{
+char name;
+float age;
+char owner;
+};
+}
