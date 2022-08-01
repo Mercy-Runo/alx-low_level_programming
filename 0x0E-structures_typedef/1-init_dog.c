d401a26ce88c00d5be92c518941c98df81d4cf66
Mercy Runo <mercyruno@gmail.com>
2022-08-01 05:40:56 -0700

    my dog is initialised

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,13 +13,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-/**
- * struct dog - dog description
- */
-struct dog
+if (d != NULL)
 {
-char name;
-float age;
-char owner;
-};
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
 }
