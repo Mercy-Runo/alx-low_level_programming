df9c39c520274bc28a4cf079ae475c1c44a4412a
Mercy Runo <mercyruno@gmail.com>
2022-08-01 05:43:02 -0700

    my dog is initialised

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,12 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t -Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* struct dog */
