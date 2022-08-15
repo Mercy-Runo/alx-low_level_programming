2c0ba4837f65255bf1c61858ef64afc59949cffa
Mercy Runo <mercyruno@gmail.com>
2022-08-15 10:38:59 -0700

    pop_listint added

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - removes first node of list
+ * @head: pointer to first element
+ * Return: head node's data or 0 if it is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr = *head;
+	int n;
+
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+	while (*head != NULL)
+	{
+		/*ptr = *head;*/
+		*head = ptr->next;
+		n = ptr->n;
+		free(ptr);
+	}
+	return (n);
+}
