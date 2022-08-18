b6afa3b77cb63e09a522f55ece10e9254971b34e
Mercy Runo <mercyruno@gmail.com>
2022-08-18 12:19:47 -0700

    changed code

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,9 +8,21 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+	int length;
+	unsigned long int mask = 1;
 
-		/* step 2 */
-		printf("%lu", n & 1);
+	length = _length(n);
+
+	if (length > 0) /*create mask based on length of number*/
+		mask <<= length; /*shift mask to the left by length*/
+
+	while (mask > 0)
+	{
+		if (n & mask) /*if n & mask == 1 print 1*/
+			_putchar('1');
+		else /*else if n & mask == 0 print 0*/
+			_putchar('0');
+
+		mask >>= 1; /*shift mask to the right by 1*/
+	}
 }
