d1ff72910520ea12b3d99a1943c547f81443bebe
Mercy Runo <mercyruno@gmail.com>
2022-08-18 05:28:32 -0700

    printbinary added

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_binary - prints binary representation of an int n
+  * @n: int to be used
+  * Return: void
+  */
+
+void print_binary(unsigned long int n)
+{
+  if (n > 1)
+        print_binary(n / 2);
+ 
+    /* step 2 */
+    printf("%lu", n % 2);
+}
