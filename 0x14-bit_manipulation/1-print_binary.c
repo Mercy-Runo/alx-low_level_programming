b01d721fc9701457c953893c2265a35880ea842e
Mercy Runo <mercyruno@gmail.com>
2022-08-18 05:30:32 -0700

    printbinary added

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,9 +8,9 @@
 
 void print_binary(unsigned long int n)
 {
-  if (n > 1)
-        print_binary(n / 2);
- 
-    /* step 2 */
-    printf("%lu", n % 2);
+	if (n > 1)
+		print_binary(n / 2);
+
+		/* step 2 */
+		printf("%lu", n % 2);
 }
